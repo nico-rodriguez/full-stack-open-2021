826926c672427c8df81937db08bb0c40eb8c817e
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-04-15 23:29:15 -0300

    Remove console.log

--- part7/bloglist-frontend/src/components/Notification.js
+++ part7/bloglist-frontend/src/components/Notification.js
@@ -1,7 +1,6 @@
 import './Notification.css';
 
 function Notification({ message, type }) {
-  console.log(message, type);
   if (message === null) return null;
 
   return <div className={`notification ${type}`}>{message}</div>;
